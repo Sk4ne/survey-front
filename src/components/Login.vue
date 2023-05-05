8f753e7458c1fa6acb1c894459e454c4e245505b
Spekth <neider86@protonmail.com>
2023-05-05 16:21:04 -0500

    Se cambio el titulo del modal registrar usuario

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -41,7 +41,7 @@
               <div class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h5 class="modal-title text-monospace" id="exampleModalLabel"><b>Registrarteddd</b></h5>
+                    <h5 class="modal-title text-monospace" id="exampleModalLabel"><b>Registrarte</b></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
                     </button>
